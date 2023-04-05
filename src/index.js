ef6bb7eb183abcebdd201c18b471c6e20098f579
imaas-wynder <87035731+imaas-wynder@users.noreply.github.com>
2023-04-05 07:07:07 -0400

    Update index.js
    Reflects April 5th changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,88 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Component } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    class Form extends Component {
+      render(){
+        return (
+          <div class="form">
+      <div class="form-group">
+        <label class="col-md-4 control-label" for="firstname">First Name</label>  
+        <div class="col-md-4">
+        <input id="firstname" name="firstname" type="text" class="form-control input-md"></input>
+        <span class="help-block">Your first name</span>  
+        </div>
+      </div>
+    
+      
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      <div class="form-group">
+        <label class="col-md-4 control-label" for="lastname">Last Name</label>  
+        <div class="col-md-4">
+        <input id="lastname" name="lastname" type="text" class="form-control input-md"></input>
+        <span class="help-block">Your last name</span>  
+        </div>
+      </div>
+      
+      <div class="form-group">
+        <label class="col-md-4 control-label" for="addinfo">Additional Information</label>
+        <div class="col-md-4">                     
+          <textarea class="form-control" id="addinfo" name="addinfo">Contract</textarea>
+        </div>
+      </div>
+      
+      <div class="form-group">
+        <label class="col-md-4 control-label" for="filebutton">File Button</label>
+        <div class="col-md-4">
+          <input id="filebutton" name="filebutton" class="input-file" type="file"></input>
+          <input type="file" name="file" onChange={this.onChangeHandler}/>
+        </div>
+      </div>
+      
+      <div class="form-group">
+        <label class="col-md-4 control-label" for="filetype">Define filetype</label>
+        <div class="col-md-4">
+          <div class="input-group">
+            <input id="filetype" name="filetype" class="form-control" placeholder="PDF" type="text">
+            <div class="input-group-btn">
+              <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                Action
+                <span class="caret"></span>
+              </button>
+              <ul class="dropdown-menu pull-right">
+                <li><a>PDF</a></li>
+                <li><a>Word</a></li>
+                <li><a>Image</a></li>
+              </ul>
+            </div>
+          </input>
+        </div>
+      </div>
+      
+      <div class="form-group">
+        <label class="col-md-4 control-label" for="submit">Submit</label>
+        <div class="col-md-4">
+          <button type="button" class="btn btn-success btn-block" onClick={this.onClickHandler}>Submit</button> 
+        </div>;
+      </div>;
+      </div>;
+      </div>
+      )
+      }
+      };
+
+     function onChangeHandler (
+
+      onClickHandler = () => {
+        onClickHandler = () => {
+          const data = new FormData() 
+          data.append('file', this.state.selectedFile)
+          POST https://api.developer.opentext.com/capture/cp-swc/home.aspx?subscription-name=b4ce7228-d27f-440b-8800-803974cabc45-cap
+  
+            Authorization: Bearer {0318LN5e3m2tx0GRlfTyL6NXCs64tipG}
+Accept: application/hal+json
+Accept-Language: en-US
+            
+        ;
+      };
+    };
+    
+     );
